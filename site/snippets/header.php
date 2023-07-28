e5e1c6017e3bcde221c054196eed27954bbef13a
RazziAftab <razziaftab@gmail.com>
2023-07-28 14:25:37 +0200

    create menu

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -6,3 +6,18 @@
   <?= css('assets/css/styles.css') ?>
 </head>
 <body>
+<header>
+
+  <?php
+  $items = $pages->listed();
+  if($items->isNotEmpty()):
+  ?>
+  <nav>
+    <ul>
+      <?php foreach($items as $item): ?>
+        <li><a<?php e($item->isOpen(), ' class="active"') ?> href="<?= $item->url() ?>"><?= $item->title()->html() ?></a></li>
+      <?php endforeach ?>
+    </ul>
+  </nav>
+  <?php endif ?>
+</header>
